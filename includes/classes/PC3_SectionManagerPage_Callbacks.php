3fa375d4b64ea836673eb0609d95ab4580b99552
Paul Craig <pcraig3@uwo.ca>
2015-01-31 18:29:56 -0500

    Slight refactoring. Now we need to save User preferences.

--- includes/classes/PC3_SectionManagerPage_Callbacks.php
+++ includes/classes/PC3_SectionManagerPage_Callbacks.php
@@ -10,6 +10,8 @@ class PC3_SectionManagerPage_Callbacks {
 
     /*
      * @TODO: Order of sections isn't preserved because of the dynamic callback thing.
+     * @TODO: Comments
+     * @TODO: Less hard-coding
      * THUS, we need some metadata keeping track of the order of the fields?
      * Maybe a hidden field that has a "{position}:{id}" storage?
      *
@@ -24,11 +26,6 @@ class PC3_SectionManagerPage_Callbacks {
      * The page slug to add the tab and form elements.
      */
     public $sPageSlug   = 'manage_sections';
-
-    /**
-     * The tab slug to add to the page.
-     *
-    public $sTabSlug    = 'callbacks';
 
     /**
      * The section slug to add to the tab.
@@ -87,6 +84,15 @@ class PC3_SectionManagerPage_Callbacks {
         add_filter( 'field_definition_PC3_SectionManagerPage_my_section_1_submit_button', array( $this, 'field_definition_PC3_SectionManagerPage_my_section_1_submit_button' ) );
     }
 
+    /**
+     * Callback method passed the submit_button field after the Sections have (or haven't) been returned
+     * If sections have been found, Submit button is usable.  Otherwise, it remains disabled
+     *
+     * Note: method follows following naming pattern: field_definition_{instantiated class name}_{section id}_{field_id}
+     *
+     * @param $aField array    the field with an id of 'submit_button'
+     * @return mixed array     the field
+     */
     public function field_definition_PC3_SectionManagerPage_my_section_1_submit_button( $aField ) {
 
         if( $this->bIfSections)
@@ -97,8 +103,14 @@ class PC3_SectionManagerPage_Callbacks {
         return $aField;
     }
 
-    /*
-     * Field callback methods - for field definitions that require heavy tasks should be defined with the callback methods.
+    /**
+     * Callback method passed the sortable text fields.  Default field value is a label saying that no Sections were found.
+     * If, however, sections are found, then field is updated with sections, and the user is free to rearrange them.
+     *
+     * Note: method follows following naming pattern: field_definition_{instantiated class name}_{section id}_{field_id}
+     *
+     * @param $aField array    the field with an id of 'callback_example'
+     * @return array array     the field
      */
     public function field_definition_PC3_SectionManagerPage_my_section_1_callback_example( $aField ) { // field_definition_{instantiated class name}_{section id}_{field_id}
 
@@ -115,19 +127,17 @@ class PC3_SectionManagerPage_Callbacks {
         $aField['description']  = sprintf( __( 'This description is inserted with the callback method: <code>%1$s</code>.', 'one-page-sections' ), __METHOD__ );
         $aField['sortable']     = true;
 
-        $oFirst_Post = array_shift( $aPosts );
+        //first section must exist because array is not empty
+        $_oFirstPost = array_shift( $aPosts );
 
         //first value must be set differently from any preceding values
-        $aField['label']     = $oFirst_Post->post_title;
-        $aField['value']     = intval( $oFirst_Post->ID );
-        $aField['attributes']     = array(
-            'readonly'  => true
-        );
+        $aField = array_merge( $aField, $this->_returnSectionArray( $_oFirstPost->post_title, $_oFirstPost->ID ) );
 
         //stop here if only one section was found
         if( empty( $aPosts ) )
             return $aField;
 
+        //return array of arrays containing Section values that will be inserted into our field
         $aFormattedPosts = $this->_formatPostsForField( $aPosts );
 
         foreach( $aFormattedPosts as $aPost ) {
@@ -138,7 +148,12 @@ class PC3_SectionManagerPage_Callbacks {
         return $aField;
     }
 
-
+    /**
+     * Function returns posts based on slug.  n our case, we're planning on returning Sections.
+     *
+     * @param string $sPostTypeSlug
+     * @return mixed
+     */
     private function _getPosts( $sPostTypeSlug='pc3_section' ) {
 
         $_aArgs         = array(
@@ -149,6 +164,13 @@ class PC3_SectionManagerPage_Callbacks {
         return $_oResults->posts;
     }
 
+    /**
+     * Iterate through Post objects, turning each into an array that an APF Field will understand
+     * Return all arrays in another array.
+     *
+     * @param $oPosts object    WordPress Post objects
+     * @return array            an array of WordPress Post arrays
+     */
     private function _formatPostsForField( $oPosts ) {
 
         $_aSectionTextFields = array();
@@ -160,6 +182,13 @@ class PC3_SectionManagerPage_Callbacks {
         return $_aSectionTextFields;
     }
 
+    /**
+     * Returns an array that an APF Field will understand
+     *
+     * @param $post_title string    the title of a post
+     * @param $label int            the id of a post
+     * @return array
+     */
     private function _returnSectionArray( $post_title, $label ) {
 
         return array (
